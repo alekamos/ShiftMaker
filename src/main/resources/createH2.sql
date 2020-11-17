34a6c930b8c3fabaa4901d245f953c477ff22d57
Alessandro Costanza <spinore@gmail.com>
2020-11-17 21:57:02 +0100

    Tentativi falliti

--- src/main/resources/createH2.sql
+++ src/main/resources/createH2.sql
@@ -1,10 +1,4 @@
-drop table if exists RUN;
 
-drop table if exists TURNI_GENERATI;
-
-drop table if exists TURNI_GENERATI_STATS;
-
-drop table if exists TURNI_GENERATI_MONITOR;
 
 create table RUN
 (
@@ -14,8 +8,8 @@ create table RUN
     DATA_INIZIO_RUN datetime null,
     DATA_FINE_RUN datetime null,
     TIPO_RUN varchar(100) null
-)
-    comment 'tabella che logga i run';
+);
+
 
 create index RUN_ID_RUN_index
     on RUN (ID_RUN);
@@ -31,8 +25,7 @@ create table TURNI_GENERATI_MONITOR
     constraint TURNI_GENERATI_MONITOR_RUN_ID_RUN_fk
         foreign key (ID_RUN) references RUN (ID_RUN)
             on delete cascade
-)
-    comment 'tabella di raccordo tra turni generati e run';
+);
 
 create index TURNI_GENERATI_MONITOR_ID_RUN_index
     on TURNI_GENERATI_MONITOR (ID_RUN);
@@ -51,8 +44,7 @@ create table TURNI_GENERATI
     PERSONA_TURNO varchar(100) null,
     constraint TURNI_GENERATI_ID_TURNO_fk
         foreign key (ID_TURNO) references TURNI_GENERATI_MONITOR (ID_TURNO) on delete cascade
-)
-    comment 'Tabella contenente i turni generati dal sw';
+);
 
 
 
@@ -60,29 +52,28 @@ create table TURNI_GENERATI
 create table TURNI_GENERATI_STATS
 (
     ID_TURNO bigint(10) primary key,
-    MEDIA_TURNI_TOT double(5,5) null,
-    SDEV_TURNI_TOT double(5,5) null,
-    MEDIA_PRES_FEST double(5,5) null,
-    SDEV_PRES_FEST double(5,5) null,
-    MEDIA_GIORNI_FEST double(5,5) null,
-    SDEV_GIORNI_FEST double(5,5) null,
-    MEDIA_GIORNI_FER double(5,5) null,
-    SDEV_GIORNI_FER double(5,5) null,
-    MEDIA_NOTTI double(5,5) null,
-    SDEV_NOTTI double(5,5) null,
-    SDEV_1_SETTIMANA double(5,5) null,
-    SDEV_2_SETTIMANA double(5,5) null,
-    SDEV_3_SETTIMANA double(5,5) null,
-    SDEV_4_SETTIMANA double(5,5) null,
-    SDEV_5_SETTIMANA double(5,5) null,
-    SCORE double(5,5) null,
+    MEDIA_TURNI_TOT DECIMAL(5,5) null,
+    SDEV_TURNI_TOT DECIMAL(5,5) null,
+    MEDIA_PRES_FEST DECIMAL(5,5) null,
+    SDEV_PRES_FEST DECIMAL(5,5) null,
+    MEDIA_GIORNI_FEST DECIMAL(5,5) null,
+    SDEV_GIORNI_FEST DECIMAL(5,5) null,
+    MEDIA_GIORNI_FER DECIMAL(5,5) null,
+    SDEV_GIORNI_FER DECIMAL(5,5) null,
+    MEDIA_NOTTI DECIMAL(5,5) null,
+    SDEV_NOTTI DECIMAL(5,5) null,
+    SDEV_1_SETTIMANA DECIMAL(5,5) null,
+    SDEV_2_SETTIMANA DECIMAL(5,5) null,
+    SDEV_3_SETTIMANA DECIMAL(5,5) null,
+    SDEV_4_SETTIMANA DECIMAL(5,5) null,
+    SDEV_5_SETTIMANA DECIMAL(5,5) null,
+    SCORE DECIMAL(5,5) null,
     SCORE_FORMULA varchar(200) null,
     DATA_INSERIMENTO datetime null,
     DATA_AGGIORNAMENTO datetime null,
         constraint TURNI_GENERATI_STATS_ID_TURNO_fk
         foreign key (ID_TURNO) references TURNI_GENERATI_MONITOR (ID_TURNO)
-)
-    comment 'statistiche collegate al turno generato';
+) ;
 
 create index TURNI_GENERATI_STATS_ID_TURNO_index
     on TURNI_GENERATI_STATS (ID_TURNO);
---
+++ src/main/resources/dropH2.sql
@@ -0,0 +1,7 @@
+drop table if exists TURNI_GENERATI_STATS;
+
+drop table if exists TURNI_GENERATI;
+
+drop table if exists TURNI_GENERATI_MONITOR;
+
+drop table if exists RUN;
